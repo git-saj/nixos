8f49b3954498afbb166d2344d2ea568969e96a50
git-saj <saj@sajbox.com>
2024-08-24 15:36:14 +0100

    initial

---
+++ modules/core/system.nix
@@ -0,0 +1,25 @@
+{ self, pkgs, lib, inputs, ...}:
+{
+  # imports = [ inputs.nix-gaming.nixosModules.default ];
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
+  nixpkgs = {
+    overlays = [
+      inputs.nur.overlay
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    wget
+    git
+  ];
+
+  time.timeZone = "Europe/London";
+  i18n.defaultLocale = "en_US.UTF-8";
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = "24.05";
+}
