306bafa5515eb50fe41381a086e7f22193a3d72d
git-saj <git-saj@sajbox.com>
2024-09-02 18:03:54 +0100

    added xrdp

--- modules/home/scripts/scripts/vm-start.sh
+++ modules/home/scripts/scripts/vm-start.sh
@@ -4,8 +4,5 @@
 vm_name="win11"
 export LIBVIRT_DEFAULT_URI="qemu:///system"
 
-# change workspace
-hyprctl dispatch workspace 7
-
 virsh start ${vm_name}
-xfreerdp /u:dev /p:dev /v:192.168.122.222 /dynamic-resolution /f +clipboard
+hyprctl dispatch exec '[workspace 7 silent] xfreerdp /u:dev /p:dev /v:192.168.122.222 /dynamic-resolution /f +clipboard'
