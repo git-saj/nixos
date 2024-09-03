0b0ee77e5f8d5c9911f42913eabd7efd8930210d
git-saj <git-saj@sajbox.com>
2024-09-03 20:10:56 +0100

    enabled bluetooth & added blueman

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }:
-{  
+{
   hardware = {
     graphics = {
       enable = true;
       extraPackages = with pkgs; [
-    
       ];
+    };
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
     };
   };
   hardware.enableRedistributableFirmware = true;
--- modules/core/services.nix
+++ modules/core/services.nix
@@ -1,10 +1,11 @@
-{ ... }: 
+{ ... }:
 {
   services = {
     gvfs.enable = true;
     gnome.gnome-keyring.enable = true;
     dbus.enable = true;
     fstrim.enable = true;
+    blueman.enable = true;
   };
   services.logind.extraConfig = ''
     # don’t shutdown when power button is short-pressed
